fe7ce443a6eed7fae9bfa1fda953fd58c2a02328
lzyjin <1247y.j@gmail.com>
2022-07-14 20:53:35 +0900

    vue cli

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,29 @@
+<!--
+  const appHeader = {
+    template: '<div>header</div>',
+    methods: {
+      addNum: function() {
+      },
+    },
+  };
+-->
+
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  // JavaScript - 인스턴스 옵션
+  methods: {
+    addNum: function() {
+
+    },
+  },
+}
+</script>
+
+<style>
+  //CSS
+</style>
